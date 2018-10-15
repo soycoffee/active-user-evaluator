6dd46ab88eafcf3bc1db41039201d05be0baf210
Ishii <yuki.ishii.business@gmail.com>
2018-10-15 22:27:44 +0900

    simplify commands

--- app/services/GitCommandExecutor.scala
+++ app/services/GitCommandExecutor.scala
@@ -23,35 +23,38 @@ class GitCommandExecutor @Inject()(implicit temporaryFileCreator: TemporaryFileC
   def temporaryFetch(httpUrl: String): Try[Path] = {
     val tempDirectoryPath = temporaryFileCreator.create().path.getParent
     val repositoryPath = tempDirectoryPath.resolve(UUID.randomUUID().toString)
-    val commands =
-      s"""${baseCommand(tempDirectoryPath)} clone --bare $httpUrl ${repositoryPath.getFileName}""" #&&
-      s"""${baseCommand(repositoryPath)} fetch --all""""
-    logger.debug(s"Execute: $commands")
-    Try(commands.!)
+    execute(
+      (baseCommand(tempDirectoryPath) ++ Seq("clone", "--bare", httpUrl, repositoryPath.getFileName.toString)) #&&
+      (baseCommand(repositoryPath) ++ Seq("fetch", "--all"))
+    )
       .map(_ => repositoryPath)
   }
 
-  def readCommitsCount(repositoryPath: Path, author: String, sinceBeforeDays: Int): Try[Int] = {
-    val commands =
-      s"""${baseCommand(repositoryPath)} rev-list --count --all --no-merges --branches --author="$author" --since=${beforeDayStringFromToday(sinceBeforeDays)}"""
-    logger.debug(s"Execute: $commands")
-    Try(commands.!!)
+  def readCommitsCount(repositoryPath: Path, author: String, sinceBeforeDays: Int): Try[Int] =
+    execute(
+      baseCommand(repositoryPath) ++ Seq("rev-list", "--count", "--all") ++ countOptions(author, sinceBeforeDays)
+    )
       .map(_.trim.toInt)
-  }
 
-  def readChangesCountSummary(repositoryPath: Path, author: String, sinceBeforeDays: Int): Try[ChangesCountSummary] = {
-    val commands =
-      s"""${baseCommand(repositoryPath)} log --numstat --pretty="%H" --no-merges --branches  --author="$author" --since=${beforeDayStringFromToday(sinceBeforeDays)}""" #|
+  def readChangesCountSummary(repositoryPath: Path, author: String, sinceBeforeDays: Int): Try[ChangesCountSummary] =
+    execute(
+      (baseCommand(repositoryPath) ++ Seq("log", "--numstat", "--pretty", """"%H"""") ++ countOptions(author, sinceBeforeDays)) #|
       "awk 'NF==3 {plus+=$1; minus+=$2} END {printf(\"%d %d\", plus, minus)}'"
-    logger.debug(s"Execute: $commands")
-    Try(commands.!!)
+    )
       .map(_.trim.split(" ") match {
         case Array(addition: String, deletion: String) => ChangesCountSummary(addition.toInt, deletion.toInt)
       })
+
+  private def execute(commands: ProcessBuilder) = {
+    logger.debug(s"Execute: $commands")
+    Try(commands.!!)
   }
 
   private def baseCommand(directoryPath: Path) =
-    s"""git -C "$directoryPath""""
+    Seq("git", "-C", s""""$directoryPath"""")
+
+  private def countOptions(author: String, sinceBeforeDays: Int) =
+    Seq("--no-merges", "--branches" , "--author", s""""$author"""", "--since", beforeDayStringFromToday(sinceBeforeDays))
 
 }
 
