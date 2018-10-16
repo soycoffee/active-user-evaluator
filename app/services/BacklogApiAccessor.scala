cc486a0445b1a917a1af96c8fed1056d92d425fc
yishii <yishii@altpaper.net>
2018-10-16 15:10:55 +0900

    add ActivityType

--- app/services/BacklogApiAccessor.scala
+++ app/services/BacklogApiAccessor.scala
@@ -50,4 +50,50 @@ object BacklogApiAccessor {
   private implicit val gitRepositoryReads: Reads[GitRepository] = Json.reads[GitRepository]
   private implicit val userReads: Reads[User] = Json.reads[User]
 
+  private abstract class ActivityType(id: Int)
+
+  private object ActivityType {
+
+    case object CreateIssue extends ActivityType(1)
+    case object UpdateIssue extends ActivityType(2)
+    case object CreateIssueComment extends ActivityType(3)
+    case object CreateWiki extends ActivityType(5)
+    case object UpdateWiki extends ActivityType(6)
+    case object CreateFile extends ActivityType(8)
+    case object UpdateFile extends ActivityType(9)
+    case object CreateGitPush extends ActivityType(12)
+    case object CreateGitRepository extends ActivityType(13)
+    case object UpdateMultiIssue extends ActivityType(14)
+    case object CreatePullRequest extends ActivityType(18)
+    case object UpdatePullRequest extends ActivityType(19)
+    case object CreatePullRequestComment extends ActivityType(20)
+    case object CreateVersion extends ActivityType(22)
+    case object UpdateVersion extends ActivityType(23)
+
+    val managementItems = Seq(
+      CreateIssue,
+      UpdateIssue,
+      CreateIssueComment,
+      UpdateMultiIssue,
+      CreateVersion,
+      UpdateVersion
+    )
+
+    val documentItems = Seq(
+      CreateWiki,
+      UpdateWiki,
+      CreateFile,
+      UpdateFile,
+    )
+
+    val implementItems = Seq(
+      CreateGitPush,
+      CreateGitRepository,
+      CreatePullRequest,
+      UpdatePullRequest,
+      CreatePullRequestComment,
+    )
+
+  }
+
 }
