7018618d2407cb3d4719522b63648b84284ba0f6
yishii <yishii@altpaper.net>
2018-10-17 13:16:38 +0900

    define OperationKey

--- app/controllers/ApiDefinitionController.scala
+++ app/controllers/ApiDefinitionController.scala
@@ -1,19 +1,44 @@
 package controllers
 
+import controllers.ApiDefinitionController.OperationKeyAuthenticated
 import dao.ApiDefinitionDao
 import javax.inject._
+import play.api.Configuration
 import play.api.libs.json.Json
+import play.api.mvc.Security.AuthenticatedBuilder
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class ApiDefinitionController @Inject()(apiDefinitionDao: ApiDefinitionDao)(implicit ec: ExecutionContext) extends InjectedController {
+class ApiDefinitionController @Inject()(apiDefinitionDao: ApiDefinitionDao, operationKeyAuthenticated: OperationKeyAuthenticated)(implicit ec: ExecutionContext) extends InjectedController {
 
-  def index: Action[AnyContent] = Action.async {
+  def index: Action[AnyContent] = operationKeyAuthenticated.async {
     apiDefinitionDao.all()
       .map(Json.toJson(_))
       .map(Ok(_))
+  }
+
+}
+
+object ApiDefinitionController {
+
+  class OperationKeyAuthenticated @Inject()(configuration: Configuration, parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+    extends AuthenticatedBuilder(
+      OperationKeyAuthenticated.authenticate(configuration),
+      parser,
+    )
+
+  object OperationKeyAuthenticated {
+
+    val QueryStringKey = "operationKey"
+    val ConfigurationKey = "apiDefinition.operationKey"
+
+    def authenticate(configuration: Configuration)(request: RequestHeader): Option[String] = {
+      val configurationOperationKey = configuration.get[String](ConfigurationKey)
+      request.getQueryString(QueryStringKey).filter(_ == configurationOperationKey)
+    }
+
   }
 
 }
