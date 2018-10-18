5c962c3459f0cc9172f3ae63678d00612b0cb85e
Ishii <yuki.ishii.business@gmail.com>
2018-10-18 23:28:10 +0900

    fix compile error

--- app/controllers/EvaluationController.scala
+++ app/controllers/EvaluationController.scala
@@ -2,16 +2,17 @@ package controllers
 
 import javax.inject._
 import models.Activity
+import play.api.Configuration
 import play.api.mvc._
-import services.{ActivityPointJudge, BacklogApiClient, UseApiDestination}
+import services.{EvaluationAggregator, UseApiDestination}
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
 class EvaluationController @Inject()(
+                                      val configuration: Configuration,
                                       val useApiDestination: UseApiDestination,
-                                      val backlogApiClient: BacklogApiClient,
-                                      val activityPointAggregator: ActivityPointJudge,
+                                      val evaluationAggregator: EvaluationAggregator,
                                       val ec: ExecutionContext,
                                     ) extends InjectedController with BaseEvaluationController {
 
