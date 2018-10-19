31f912aeef88e8ea8c253c3cb27d01814a6af904
Ishii <yuki.ishii.business@gmail.com>
2018-10-20 00:31:18 +0900

    add EvaluationAggregatorSpec

--- app/services/ActivityPointJudge.scala
+++ app/services/ActivityPointJudge.scala
@@ -8,7 +8,8 @@ class ActivityPointJudge {
 
   def apply(activity: Activity): Int =
     activity.`type` match {
-      case Activity.Type.CreateGitPush => 0
+      case Activity.Type.CreateGitPush =>
+        activity.content.\("revision_count").validate[Int].getOrElse(1)
       case _ => 1
     }
 
--- test/services/EvaluationAggregatorSpec.scala
+++ test/services/EvaluationAggregatorSpec.scala
@@ -8,6 +8,7 @@ import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
 import play.api.test.Helpers._
 import play.api.{Application, Configuration}
 
@@ -39,8 +40,8 @@ class EvaluationAggregatorSpec extends PlaySpec with GuiceOneServerPerSuite with
   private def initializeMock(users: Seq[User], activities: Seq[Activity]) = {
     import ArgumentMatchers.any
     val backlogApiClient = mock[BacklogApiClient]
-    Mockito.when(backlogApiClient.queryProjectUsers(any())) thenReturn Future.successful(users)
-    Mockito.when(backlogApiClient.queryUserActivities(any(), any())) thenReturn Future.successful(activities)
+    Mockito.when(backlogApiClient.queryProjectUsers(any())(any())) thenReturn Future.successful(users)
+    Mockito.when(backlogApiClient.queryUserActivities(any(), any())(any())) thenReturn Future.successful(activities)
     backlogApiClient
   }
 
@@ -51,20 +52,95 @@ class EvaluationAggregatorSpec extends PlaySpec with GuiceOneServerPerSuite with
       noActivityArranger,
     )
 
-  "queryProjectUsers" in {
-    val backlogApiClient = initializeMock(
-      Seq(),
-      Seq(),
+  "queryEvaluationUsers" in {
+    val mockUsers = Seq(
+      User(1, null, null),
+      User(2, null, null),
     )
+    val mockActivities = Seq(
+      Activity(Activity.Type.CreateIssue, null, null),
+      Activity(Activity.Type.UpdateIssue, null, null),
+      Activity(Activity.Type.CreateIssueComment, null, null),
+    )
+    val argActivityTypes = Seq(
+      Activity.Type.CreateWiki,
+      Activity.Type.UpdateWiki,
+      Activity.Type.CreateFile,
+      Activity.Type.UpdateFile,
+    )
+    val backlogApiClient = initializeMock(mockUsers, mockActivities)
     val evaluationAggregator = initializeTarget(backlogApiClient)
-    evaluationAggregator.queryEvaluationUsers(1, Some())
+    val evaluationUsers = await(evaluationAggregator.queryEvaluationUsers("1", None, argActivityTypes))
+    evaluationUsers must have length 2
+    evaluationUsers.map(_.user) mustBe mockUsers
+    evaluationUsers.map(_.point) mustBe Seq.fill(2)(12)
+    evaluationUsers.map(_.activities) foreach { evaluationActivities =>
+      evaluationActivities must have length 12
+      evaluationActivities.map(_.activity) mustBe Seq.fill(4)(mockActivities).flatten
+      evaluationActivities.map(_.point) mustBe Seq.fill(12)(1)
+    }
+    Mockito.verify(backlogApiClient)
+      .queryProjectUsers(ArgumentMatchers.eq("1"))(ArgumentMatchers.eq(destination))
+    Mockito.verify(backlogApiClient, Mockito.times(8))
+      .queryUserActivities(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.eq(destination))
+    mockUsers foreach { mockUser =>
+      argActivityTypes foreach { argActivityType =>
+        Mockito.verify(backlogApiClient)
+          .queryUserActivities(ArgumentMatchers.eq(mockUser.id), ArgumentMatchers.eq(argActivityType))(ArgumentMatchers.eq(destination))
+      }
+    }
+  }
 
-    val users = await(backlogApiClient.queryProjectUsers("1"))
-    users mustBe Seq(
-      User(1, "userId", "name"),
+  "queryEvaluationUsers users = Nil" in {
+    val mockUsers = Nil
+    val mockActivities = Seq(
+      Activity(Activity.Type.CreateIssue, null, null),
     )
-    Mockito.verify(wsClient).url(ArgumentMatchers.eq("https://example.com/api/v2/projects/1/users?apiKey=key"))
-    Mockito.verify(wsRequest).withMethod(ArgumentMatchers.eq("GET"))
+    val argActivityTypes = Seq(
+      Activity.Type.CreateWiki,
+    )
+    val backlogApiClient = initializeMock(mockUsers, mockActivities)
+    val evaluationAggregator = initializeTarget(backlogApiClient)
+    val evaluationUsers = await(evaluationAggregator.queryEvaluationUsers("1", None, argActivityTypes))
+    evaluationUsers mustBe Nil
+    Mockito.verify(backlogApiClient, Mockito.never())
+      .queryUserActivities(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.eq(destination))
+  }
+
+  "queryEvaluationUsers activityTypes = Nil" in {
+    val mockUsers = Seq(
+      User(1, null, null),
+    )
+    val mockActivities = Seq(
+      Activity(Activity.Type.CreateIssue, null, null),
+    )
+    val argActivityTypes = Nil
+    val backlogApiClient = initializeMock(mockUsers, mockActivities)
+    val evaluationAggregator = initializeTarget(backlogApiClient)
+    val evaluationUsers = await(evaluationAggregator.queryEvaluationUsers("1", None, argActivityTypes))
+    evaluationUsers must have length 1
+    evaluationUsers.head.user mustBe mockUsers.head
+    evaluationUsers.head.point mustBe 0
+    evaluationUsers.head.activities mustBe Nil
+    Mockito.verify(backlogApiClient, Mockito.never())
+      .queryUserActivities(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.eq(destination))
+  }
+
+  "queryEvaluationUsers activityTypes = Seq(CreateGitPush)" in {
+    val mockUsers = Seq(
+      User(1, null, null),
+    )
+    val mockActivities = Seq(
+      Activity(Activity.Type.CreateGitPush, null, Json.obj(
+        "revision_count" -> 2,
+      )),
+    )
+    val argActivityTypes = Seq(Activity.Type.CreateGitPush)
+    val backlogApiClient = initializeMock(mockUsers, mockActivities)
+    val evaluationAggregator = initializeTarget(backlogApiClient)
+    val evaluationUsers = await(evaluationAggregator.queryEvaluationUsers("1", None, argActivityTypes))
+    evaluationUsers must have length 1
+    evaluationUsers.head.point mustBe 2
   }
 
 }
