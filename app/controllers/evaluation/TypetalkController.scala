94aa88d3acf8a1200326d87e3a212b7ad73792f2
yishii <yishii@altpaper.net>
2018-10-23 14:48:45 +0900

    refactor parse WebhookBody

--- app/controllers/evaluation/TypetalkController.scala
+++ app/controllers/evaluation/TypetalkController.scala
@@ -2,53 +2,40 @@ package controllers.evaluation
 
 import models._
 import play.api.i18n.Lang
-import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 import play.api.mvc._
 import services.{BacklogApiClient, EvaluationAggregator, UseApiDestination}
 
 import scala.Function.tupled
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 trait TypetalkController extends BaseController with HasTargetActivityTypes {
+
+  import controllers.evaluation.TypetalkController.WebhookBody
+  import WebhookBody.reads
 
   val useApiDestination: UseApiDestination
   val evaluationAggregator: EvaluationAggregator
   implicit val ec: ExecutionContext
 
-  private implicit lazy val lang: Lang = supportedLangs.availables.head
-
   def typetalkMessageLabelKey: String
 
-  def typetalkWebhook(projectId: String, apiKey: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
-    withRequestParameter { (postMessage, replyFrom) =>
-      val (count, sinceBeforeDays) = extractMessageParameter(postMessage)
-      useApiDestination(apiKey) { implicit destination =>
-        evaluationAggregator.queryEvaluationUsers(projectId, targetActivityTypes, count, sinceBeforeDays)
-          .map(buildResponseBody(_, replyFrom))
-          .map(Json.toJson(_))
-          .map(Ok(_))
-      }
-    } recoverTotal { error =>
-      Future.successful(BadRequest(error.toString))
+  private implicit lazy val lang: Lang = supportedLangs.availables.head
+
+  def typetalkWebhook(projectId: String, apiKey: String): Action[WebhookBody] = Action.async(parse.json[WebhookBody]) { implicit request =>
+    val WebhookBody(count, sinceBeforeDays, replyFrom) = request.body
+    useApiDestination(apiKey) { implicit destination =>
+      evaluationAggregator.queryEvaluationUsers(projectId, targetActivityTypes, count, sinceBeforeDays)
+        .map(buildResponseBody(_, replyFrom))
+        .map(Json.toJson(_))
+        .map(Ok(_))
     }
   }
 
   lazy val typetalkMessageLabel : String =
     messagesApi(typetalkMessageLabelKey)
-
-  private def withRequestParameter[T](f: (String, Long) => T)(implicit request: Request[JsValue]) = {
-    for {
-      postMessage <- (request.body \ "post" \ "message").validate[String]
-      replyFrom <- (request.body \ "post" \ "account" \ "id").validate[Long]
-    } yield f(postMessage, replyFrom)
-  }
-
-  private def extractMessageParameter[T](message: String) = {
-    val pickInt = (key: String) =>
-      s"""$key=\\S+""".r.findFirstIn(message).flatMap(Try(_).map(_.split("=")(1).toInt).toOption)
-    (pickInt("count"), pickInt("sinceBeforeDays"))
-  }
 
   private def buildResponseBody(evaluationUsers: Seq[EvaluationUser], replyTo: Long)(implicit destination: BacklogApiClient.Destination): JsObject =
     Json.obj(
@@ -62,6 +49,27 @@ trait TypetalkController extends BaseController with HasTargetActivityTypes {
     ))
       .+:(typetalkMessageLabel)
       .mkString("\n")
+
+}
+
+object TypetalkController {
+
+  case class WebhookBody(count: Option[Int], sinceBeforeDays: Option[Int], replyFrom: Long)
+
+  object WebhookBody {
+
+    def apply(message: String, replyFrom: Long): WebhookBody = {
+      val pickInt = (key: String) =>
+        s"""$key=\\S+""".r.findFirstIn(message).flatMap(Try(_).map(_.split("=")(1).toInt).toOption)
+      WebhookBody(pickInt("count"), pickInt("sinceBeforeDays"), replyFrom)
+    }
+
+    implicit val reads: Reads[WebhookBody] = (
+      (__ \ "post" \ "message").read[String] and
+        (__ \ "post" \ "account" \ "id").read[Long]
+      ) (WebhookBody(_: String, _: Long))
+
+  }
 
 }
 
--- test/controllers/evaluation/TypetalkControllerSpec.scala
+++ test/controllers/evaluation/TypetalkControllerSpec.scala
@@ -1,9 +1,10 @@
 package controllers.evaluation
 
+import controllers.evaluation.TypetalkController.WebhookBody
 import models.{EvaluationActivity, EvaluationUser, User}
 import org.mockito.Mockito
 import play.api.http.Status
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.Json
 import play.api.mvc._
 import play.api.test.Helpers._
 import play.api.test._
@@ -11,22 +12,13 @@ import test.helpers.NoSlick
 
 trait TypetalkControllerSpec[Controller <: TypetalkController with InjectedController] extends BaseSpec[Controller] with NoSlick {
 
-  private def initializeMock(requestBody: JsValue, evaluationUsers: Seq[EvaluationUser]) = {
+  private def initializeMock(requestBody: WebhookBody, evaluationUsers: Seq[EvaluationUser]) = {
     val (useApiDestination, evaluationAggregator) = super.initializeMock(evaluationUsers)
-    val request = mock[Request[JsValue]]
+    val request = mock[Request[TypetalkController.WebhookBody]]
     Mockito.when(request.body) thenReturn requestBody
+    Mockito.when(request.contentType) thenReturn Some("application/json")
     (request, useApiDestination, evaluationAggregator)
   }
-
-  private def buildRequestBody(postMessage: String, replyFrom: Long) =
-    Json.obj(
-      "post" -> Json.obj(
-        "message" -> postMessage,
-        "account" -> Json.obj(
-          "id" -> replyFrom,
-        ),
-      )
-    )
 
   "typetalkWebhook" should {
 
@@ -40,7 +32,7 @@ trait TypetalkControllerSpec[Controller <: TypetalkController with InjectedContr
 
     "OK" in {
       val (request, useApiDestination, evaluationAggregator) = initializeMock(
-        buildRequestBody("", 1),
+        WebhookBody("", 1),
         Seq(
           buildEvaluationUser("a", "A", 3),
           buildEvaluationUser("b", "B", 2),
@@ -61,17 +53,6 @@ trait TypetalkControllerSpec[Controller <: TypetalkController with InjectedContr
         "replyTo" -> 1,
       )
       Mockito.verify(evaluationAggregator).queryEvaluationUsers("projectId", targetActivityTypes, None, None)(apiDestination)
-    }
-
-    "parseMessage" in {
-      val (request, useApiDestination, evaluationAggregator) = initializeMock(
-        buildRequestBody("count=1 sinceBeforeDays=2", 1),
-        Seq(),
-      )
-      val controller = initializeTarget(useApiDestination, evaluationAggregator)
-      val result = controller.typetalkWebhook("projectId", "apiKey")(request)
-      Helpers.status(result) mustBe Status.OK
-      Mockito.verify(evaluationAggregator).queryEvaluationUsers("projectId", targetActivityTypes, Some(1), Some(2))(apiDestination)
     }
 
   }
