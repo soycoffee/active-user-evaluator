aadc524ce3bc7bf4a9ef878c5f1aad46358cc606
yishii <yishii@altpaper.net>
2018-10-24 16:10:10 +0900

    fix test

--- test/routes/ApiDefinitionControllerSpec.scala
+++ test/routes/ApiDefinitionControllerSpec.scala
@@ -43,7 +43,7 @@ class ApiDefinitionControllerSpec extends PlaySpec with GuiceOneServerPerSuite {
       routes.ApiDefinitionController.query(),
       routes.ApiDefinitionController.create(),
       routes.ApiDefinitionController.update(),
-      routes.ApiDefinitionController.delete(""),
+      routes.ApiDefinitionController.delete("x"),
     )) {
 
       s"${call.method} ${call.url}" in {
